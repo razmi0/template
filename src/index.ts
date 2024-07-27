bf3c7659aaeee2109cd4c31553ae2911730edf11
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-27 11:20:27 +0200

    Add setAnims function to apply or reset animations on elements

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,44 @@
-const wrapper = document.querySelector(".wrapper") as HTMLDivElement;
-const items = Array.from(wrapper.querySelectorAll("div")) as HTMLDivElement[];
+import { Nsize, defColor, itemsData } from "./constant";
+import { isDefined } from "./utils";
 
-const defColor = "#e8e8e8";
-const colors = ["#9b5de5", "#f15bb5", "#fee440", "#00bbf9", "#00f5d4"];
-const itemsData = items.map((item, i) => {
-  return {
-    start: item.getBoundingClientRect().left,
-    end: item.getBoundingClientRect().right,
-    color: colors[i],
-    element: item,
-  };
-});
+const setAnims = (
+  { from, to, except, on }: { from?: number; to?: number; except?: number; on?: number },
+  action: "apply" | "reset" | undefined = "apply"
+) => {
+  const getColor = (i: number) => (action === "apply" ? itemsData[i].color : defColor);
+  const getHeight = () => (action === "apply" ? "100%" : "100px");
 
-const leftStart = itemsData[0].start;
-const rightEnd = itemsData[itemsData.length - 1].end;
-
-console.log(itemsData);
+  if (isDefined(on)) {
+    itemsData[on].element.style.backgroundColor = getColor(on);
+    itemsData[on].element.style.height = getHeight();
+    return;
+  }
+  if (isDefined(from) && isDefined(to)) {
+    if (from < 0 || to > Nsize) return;
+    for (let i = from; i <= to; i++) {
+      if (i === except) continue;
+      itemsData[i].element.style.backgroundColor = getColor(i);
+      itemsData[i].element.style.height = getHeight();
+    }
+  }
+};
 
 document.addEventListener("mousemove", (e) => {
   const x = e.clientX;
-  itemsData.forEach((item) => {
-    const { start, end, color, element } = item;
-    if (x > start && x < end) {
-      element.style.backgroundColor = color;
-      element.style.height = "100%";
-    } else if (x < leftStart) {
-      itemsData[0].element.style.backgroundColor = itemsData[0].color;
-      itemsData[0].element.style.height = "100%";
-    } else if (x > rightEnd) {
-      itemsData[itemsData.length - 1].element.style.backgroundColor = itemsData[itemsData.length - 1].color;
-      itemsData[itemsData.length - 1].element.style.height = "100%";
-    } else {
-      element.style.backgroundColor = defColor;
-      element.style.height = "100px";
+  if (x <= itemsData[0].end) {
+    setAnims({ on: 0 });
+    setAnims({ from: 1, to: Nsize - 1 }, "reset");
+    return;
+  } else if (x >= itemsData[Nsize - 1].start) {
+    setAnims({ on: Nsize - 1 });
+    setAnims({ from: 0, to: Nsize - 2 }, "reset");
+    return;
+  }
+  for (let i = 0; i < Nsize - 1; i++) {
+    if (x >= itemsData[i].start && x <= itemsData[i].end) {
+      setAnims({ on: i });
+      setAnims({ from: 0, to: Nsize - 1, except: i }, "reset");
+      return;
     }
-  });
+  }
 });
