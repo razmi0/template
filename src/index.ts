e981ca7ea1139bfd444568128ed4b305f6e9d729
razmi0 <121302828+razmi0@users.noreply.github.com>
2024-07-27 16:50:52 +0200

    Decouple setAnim and animation functions for multiple animations

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,101 @@
-import { Nsize, defColor, itemsData } from "./constant";
+import { animations } from "./animations";
+import { colors } from "./colors";
+import { items, type ItemData } from "./items";
 import { isDefined } from "./utils";
 
+type SetAnimationOptions = {
+  from?: number;
+  to?: number;
+  except?: number[];
+  on?: number;
+  animType: number;
+};
+
 const setAnims = (
-  { from, to, except, on }: { from?: number; to?: number; except?: number; on?: number },
+  { from, to, except, on, animType }: SetAnimationOptions,
   action: "apply" | "reset" | undefined = "apply"
 ) => {
-  const getColor = (i: number) => (action === "apply" ? itemsData[i].color : defColor);
-  const getHeight = () => (action === "apply" ? "100%" : "100px");
+  const data = items[animType] as ItemData[];
+
+  const getColor = (i: number) => (action === "apply" ? data[i].color : colors.default);
+  const getAnimation = () => (action === "apply" ? animations[animType].active : animations[animType].initial);
 
   if (isDefined(on)) {
-    itemsData[on].element.style.backgroundColor = getColor(on);
-    itemsData[on].element.style.height = getHeight();
+    data[on].element.style.backgroundColor = getColor(on);
+    const animation = getAnimation();
+
+    for (const key in animation) {
+      // @ts-ignore
+      data[on].element.style[key] = animation[key];
+    }
+
     return;
   }
   if (isDefined(from) && isDefined(to)) {
-    if (from < 0 || to > Nsize) return;
+    if (from < 0 || to > data.length) return;
     for (let i = from; i <= to; i++) {
-      if (i === except) continue;
-      itemsData[i].element.style.backgroundColor = getColor(i);
-      itemsData[i].element.style.height = getHeight();
+      if (except && except.some((exception) => i === exception)) continue;
+      data[i].element.style.backgroundColor = getColor(i);
+      const animation = getAnimation();
+
+      for (const key in animation) {
+        // @ts-ignore
+        data[i].element.style[key] = animation[key];
+      }
+    }
+  }
+};
+
+const growFromCenter = (x: number) => {
+  const animType = 0;
+  const data = items[animType];
+  const size = data.length;
+  if (x <= data[0].end) {
+    setAnims({ on: 0, animType });
+    setAnims({ from: 1, to: size - 1, animType }, "reset");
+    return;
+  } else if (x >= data[size - 1].start) {
+    setAnims({ on: size - 1, animType });
+    setAnims({ from: 0, to: size - 2, animType }, "reset");
+    return;
+  }
+  for (let i = 0; i < size - 1; i++) {
+    if (x >= data[i].start && x <= data[i].end) {
+      setAnims({ on: i, animType });
+      setAnims({ from: 0, to: size - 1, except: [i], animType }, "reset");
+      return;
+    }
+  }
+};
+
+const growToCenter = (y: number) => {
+  let animType = 1;
+  const data = items[animType];
+  const size = data.length;
+
+  if (y <= data[0].bottom) {
+    setAnims({ from: 0, to: 1, animType });
+    setAnims({ from: 2, to: size - 1, animType }, "reset");
+    return;
+  } else if (y >= data[size - 1].top) {
+    setAnims({ from: size - 2, to: size - 1, animType });
+    setAnims({ from: 0, to: size - 3, animType }, "reset");
+    return;
+  }
+  for (let i = 0; i < size - 1; i++) {
+    if (y >= data[i].top && y <= data[i].bottom) {
+      setAnims({ from: i, to: i + 1, animType: 1 });
+      setAnims({ from: 0, to: size - 1, except: [i, i + 1], animType: 1 }, "reset");
+      return;
     }
   }
 };
 
 document.addEventListener("mousemove", (e) => {
-  const x = e.clientX;
-  if (x <= itemsData[0].end) {
-    setAnims({ on: 0 });
-    setAnims({ from: 1, to: Nsize - 1 }, "reset");
-    return;
-  } else if (x >= itemsData[Nsize - 1].start) {
-    setAnims({ on: Nsize - 1 });
-    setAnims({ from: 0, to: Nsize - 2 }, "reset");
-    return;
-  }
-  for (let i = 0; i < Nsize - 1; i++) {
-    if (x >= itemsData[i].start && x <= itemsData[i].end) {
-      setAnims({ on: i });
-      setAnims({ from: 0, to: Nsize - 1, except: i }, "reset");
-      return;
-    }
-  }
+  const x = e.pageX;
+  const y = e.pageY;
+  /** SECTION 1 */
+  growFromCenter(x);
+  /** SECTION 2 */
+  growToCenter(y);
 });
